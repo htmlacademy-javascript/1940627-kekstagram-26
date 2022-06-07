194b2c4f1fe389ae8f2becfa2909278f6d37b34f
T11x <T11x11@gmail.com>
2022-06-07 16:27:22 +0300

    Замена функций на стрелочные

--- js/main.js
+++ js/main.js
@@ -2,18 +2,16 @@
   https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   + добавил взятие чисел по модулю, для обработки отрицательных значений. */
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   min = Math.ceil(Math.abs(min));
   max = Math.floor(Math.abs(max));
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRandomInt(3, 19);
 
 //Функция для проверки максимальной длины строки.
 
-function checkCommentLength(comment, maxLength) {
-  return comment.length <= maxLength;
-}
+const checkCommentLength = (comment, maxLength) => comment.length <= maxLength;
 
 checkCommentLength('Hello world', 11);
